d79105d6a77edd858c15cc6975a0fe39bd81eadc
Caleb <calsutton728@gmail.com>
2018-05-09 02:02:41 -0500

    fixed build_rules(), print_rules()

--- Apriori_frozenset.py
+++ Apriori_frozenset.py
@@ -39,20 +39,53 @@ def apriori(transactions, initial_candidates_list, min_support):
 
 # Does not work. need a better way of getting support for items
 
-def build_rules(large_itemsets, transactions, min_support, min_confidence):
+def build_rules(large_itemsets, min_confidence):
     rules = []
-    min_c = 0.6
     
+    individual_items = []
+    for item in large_itemsets[0]:
+        individual_items.append(item)
+
+    large_itemsets.pop(0)
+
     for k_value in large_itemsets:
-        for itemset in large_itemsets:
-            for x in itemset:
-                confidence = get_support(transactions, itemset, min_support)/get_support(transactions, x, min_support) 
-                if  confidence > min_confidence:
-                    right = itemset
-                    right.remove(x)
-                    rules.append({'left': x, 'right': right})
+        for itemset in k_value:
+            list_of_frozen_sets = []
+            for individual_item in individual_items:
+                # print(individual_item[0], ' in ', itemset[0], '? ', individual_item[0].issubset(itemset[0]))
+                if individual_item[0].issubset(itemset[0]):
+                    list_of_frozen_sets.append(individual_item)
+            # print(itemset, ': ', list_of_frozen_sets)
+
+            for item_ in list_of_frozen_sets:
+                confidence = itemset[1] / item_[1]
+                if confidence > min_confidence:
+                    rules.append({'left': item_[0], 'right': itemset[0].difference(item_[0]), 'confidence': confidence})
+
+    # for rule in rules:             
+    #     print(rule)
 
     return rules
+
+def print_rules(rules, input_file):
+    products = []
+
+    with open(input_file, 'r', encoding = 'utf8') as f:
+        for line in f:
+            products.append(line.strip().rstrip(',').split(','))
+    
+    for product in products:
+        product[0] = frozenset([product[0]])
+
+        for rule in rules:
+            if product[0].issubset(rule['left']):
+                rule['left'] = product[1]
+            elif product[0].issubset(rule['right']):
+                rule['right'] = product[1]
+    
+    for rule in rules:
+        print(rule['left'], '=>', rule['right'], 'with', str(round(rule['confidence'] * 100, 2)) + '%', 'confidence.')
+
 
 # Function which calculates the support for each item
 # and returns an array of items which meet the min support.
@@ -67,9 +100,9 @@ def get_support(transactions, itemset, min_support):
             if item.issubset(transaction):
                 count +=1
         # Calculate support, based on the length of transactions.
-        support = count / (len(transactions))
+        support = count
         # If support meets criteria, add to set and return.
-        if support >= min_support:
+        if support / len(transactions) >= min_support:
             new_itemset.append([(item), support])      
     return (new_itemset)
 
@@ -137,18 +170,30 @@ def read_data(input_file):
             initial_candidates.add(frozenset([item]))
     return transactions , initial_candidates
 
+
+
 def main():
     if len(sys.argv) > 2:
         start_time = time.time()
 
         file_name = sys.argv[1]                               # Accepts filename as cmd line argument.
         support_value = float(sys.argv[2])
+        confidence_value = float(sys.argv[3])
         transactions, initial_candidates = read_data(file_name)
 
         L_set = apriori(transactions, initial_candidates, support_value)
         # build_rules(L_set)
-        print (L_set)
+        # print (L_set)
 
+        # for k in L_set:
+        #     for l in k:
+        #         if len(l[0]) > 1:
+        #             print(l)
+
+        print()
+        print()
+
+        print_rules(build_rules(L_set, confidence_value), 'products.csv')
 
         # Progress bar......
         # for i in tqdm(range(10)):
@@ -161,6 +206,6 @@ def main():
 
     else:
         print ("Please enter the correct cmd line arguments in the format:")
-        print ("python Apriori.py  data.csv  support_decimal")
+        print ("python Apriori.py  data.csv  support_decimal confidence_decimal")
     
 main()
